431b31c280857c852990a4c67db55fc2009ed2d8
Saurabh Pal <118064701+saurabh27pal@users.noreply.github.com>
2023-02-02 19:18:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your code here
+let str;
+document.getElementById("evaluatedText").value = ""; // initially string is empty so count is also empty
+
+document.addEventListener("keyup", function () {
+	  str = document.getElementById("evaluatedText").value; //here this value will return string
+	// consostr.split(' ').length;
+		 // str = str.replace(/(^\s*)|(\s*$)/gi,"");  // Exclude the start and end spaces of a string
+   //       str = str.replace(/[ ]{2,}/gi," "); // reduce multiple spaces to a single space.
+ let x = str.split(' ')
+	x= x.filter(item => item);
+	console.log(x)
+	document.querySelector("#wordCount").textContent = x.length;
+	
+    // console.log(str);
+	// console.log(str.split(' ').length);
+});
+
+// function handle() {
+//   let count = document.getElementById("evaluatedText").value; //here this value will return string
+//   console.log(count);
+//   document.querySelector("#letterCount").textContent = count.length;
+// }
